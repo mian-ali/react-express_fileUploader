00553b6e47ea944712b3ddee51b0e54172e174a7
Boss <aliahmadoffical55@gmail.com>
2022-04-05 05:17:58 +0500

    starter project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FileUpload from './component/fileUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Main Component</h1>
+
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
